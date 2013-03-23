3b3c473730bf43389396a262d4774c0bacf90622
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2013-03-23 15:38:43 +0100

    IHMPlateau: Cabanes + Chemins

--- Modele_de_jeu/src/ihm/IHMCabane.java
+++ Modele_de_jeu/src/ihm/IHMCabane.java
@@ -1,5 +1,21 @@
 package ihm;
 
-public class IHMCabane {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class IHMCabane extends JButton {
+	IHMCabane() {
+		super("Cabane");
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(105, 105));
+		setBackground(Color.BLACK);
+		setForeground(Color.ORANGE);
+		setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+		setOpaque(true);
+	}
 }
--- Modele_de_jeu/src/ihm/IHMPlateau.java
+++ Modele_de_jeu/src/ihm/IHMPlateau.java
@@ -14,33 +14,22 @@ public class IHMPlateau extends JPanel{
 	IHMPlateau() {
 		setLayout(new BorderLayout());
 		JPanel chemins = new JPanel();
-		JPanel cabane1 = new JPanel();
-		JPanel cabane2 = new JPanel();
-		JPanel cabane3 = new JPanel();
-		JPanel cabane4 = new JPanel();
+		ArrayList<JPanel> bords = new ArrayList<JPanel>();
+		ArrayList<IHMCabane> cabanes = new ArrayList<IHMCabane>();
 		
-		cabane1.setBackground(Color.ORANGE);
-		cabane1.setOpaque(true);
-		JLabel cabaneA = new JLabel("cabane");
-		cabane1.setLayout(new FlowLayout(FlowLayout.CENTER));
-		cabane1.add(cabaneA);
 		
-		cabane2.setBackground(Color.ORANGE);
-		cabane2.setOpaque(true);
-		cabane3.setBackground(Color.ORANGE);
-		cabane3.setOpaque(true);
-		cabane4.setBackground(Color.ORANGE);
-		cabane4.setOpaque(true);
-		this.add(cabane1, BorderLayout.NORTH);
-		this.add(cabane2, BorderLayout.EAST);
-		this.add(cabane3, BorderLayout.SOUTH);
-		this.add(cabane4, BorderLayout.WEST);
+		for(int i=0; i<4; i++){
+			cabanes.add(new IHMCabane());
+			bords.add(new JPanel());
+			bords.get(i).setLayout(new FlowLayout(FlowLayout.CENTER));
+			bords.get(i).add(cabanes.get(i));
+		}
 		
 		
 		chemins.setLayout(new FlowLayout());
 		
 		ArrayList<IHMCase> cases = new ArrayList<IHMCase>();
-    	for(int i=0; i<10; i++){
+    	for(int i=0; i<11; i++){
     		cases.add(new IHMCase());
     	}
     	
@@ -49,9 +38,9 @@ public class IHMPlateau extends JPanel{
     	}
     	
     	this.add(chemins, BorderLayout.CENTER);
-    
-		
-		this.setBackground(Color.BLUE);
-		this.setOpaque(true);
+    	this.add(bords.get(0), BorderLayout.NORTH);
+		this.add(bords.get(2), BorderLayout.SOUTH);
+		this.add(bords.get(1), BorderLayout.EAST);
+		this.add(bords.get(3), BorderLayout.WEST);
 	}
 }
