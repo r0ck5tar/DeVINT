62fe1f22bbb615e5c2360ef21a02b53e3d691957
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2013-03-23 14:59:44 +0100

    IHMPlateau: Proof of Concept
    Just to get an idea of what IHMPlateau will look like

--- Modele_de_jeu/src/ihm/IHMPlateau.java
+++ Modele_de_jeu/src/ihm/IHMPlateau.java
@@ -2,6 +2,8 @@ package ihm;
 
 import java.awt.*;
 import java.util.ArrayList;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -10,7 +12,32 @@ import javax.swing.JPanel;
 public class IHMPlateau extends JPanel{
 	
 	IHMPlateau() {
-		setLayout(new FlowLayout());
+		setLayout(new BorderLayout());
+		JPanel chemins = new JPanel();
+		JPanel cabane1 = new JPanel();
+		JPanel cabane2 = new JPanel();
+		JPanel cabane3 = new JPanel();
+		JPanel cabane4 = new JPanel();
+		
+		cabane1.setBackground(Color.ORANGE);
+		cabane1.setOpaque(true);
+		JLabel cabaneA = new JLabel("cabane");
+		cabane1.setLayout(new FlowLayout(FlowLayout.CENTER));
+		cabane1.add(cabaneA);
+		
+		cabane2.setBackground(Color.ORANGE);
+		cabane2.setOpaque(true);
+		cabane3.setBackground(Color.ORANGE);
+		cabane3.setOpaque(true);
+		cabane4.setBackground(Color.ORANGE);
+		cabane4.setOpaque(true);
+		this.add(cabane1, BorderLayout.NORTH);
+		this.add(cabane2, BorderLayout.EAST);
+		this.add(cabane3, BorderLayout.SOUTH);
+		this.add(cabane4, BorderLayout.WEST);
+		
+		
+		chemins.setLayout(new FlowLayout());
 		
 		ArrayList<IHMCase> cases = new ArrayList<IHMCase>();
     	for(int i=0; i<10; i++){
@@ -18,17 +45,11 @@ public class IHMPlateau extends JPanel{
     	}
     	
     	for(int i=0; i<cases.size(); i++){
-    		this.add(cases.get(i));
+    		chemins.add(cases.get(i));
     	}
     	
-		int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-		int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-		Dimension maxSize = new Dimension (screenHeight, screenHeight); 
-		
-		this.setBounds((screenWidth-screenHeight)/2, 0, screenHeight, screenHeight);
-		
-		this.setMaximumSize(maxSize);
-		this.setPreferredSize(maxSize);
+    	this.add(chemins, BorderLayout.CENTER);
+    
 		
 		this.setBackground(Color.BLUE);
 		this.setOpaque(true);
