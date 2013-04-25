b3bf43be4cd604177307a69cf92f61c161ff8d78
user <user@user-Latitude-E5430-non-vPro.(none)>
2013-04-25 16:06:57 +0200

    correction

--- Modele_de_jeu/src/Tool/Tool.java
+++ Modele_de_jeu/src/Tool/Tool.java
@@ -6,8 +6,10 @@ import java.util.Random;
 import fonctionnement.environnement.Case;
 import fonctionnement.objet.Joueur;
 import fonctionnement.objet.ObjetEffet;
+import fonctionnement.objet.Ressource;
 
 public class Tool {
+
 	public static int lancerDe(int n) {
 		Random rnd =  new Random();
 		return rnd.nextInt(n);
@@ -18,6 +20,35 @@ public class Tool {
 		return null;
 	}
 	
+	public static void parler(String phrase) {
+		
+	}
+	
+	public static ArrayList<Case> getChoixCase(int n,Joueur joueur) {
+		return null;
+	}
+	
+	public static Ressource recupRessource(Ressource r, Joueur j) {
+		return null;
+	}
+	
+	public static Joueur mettreRessource(Ressource r, Joueur j) {
+		return null;
+	}
+	
+	public static boolean containsObjetEffet(Joueur j) {
+		return false;
+	}
+	
+	public static ArrayList<ObjetEffet> recupObjetSpecial(Joueur j) {
+		return null;
+	}
+	
+	public static boolean appliquerEffet(Joueur j, ObjetEffet o) {
+		// return true si deplacement
+		// return false si pas de deplacement
+		return false;
+	}
 	
 	
 	public static void viderSac(Joueur j){
