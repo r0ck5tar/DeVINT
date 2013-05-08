44788df24d4c70aabc0993c041200917d706aae1
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2013-05-08 13:12:44 +0200

    Removed the IHMCabane

--- Modele_de_jeu/src/ihm/IHMCabane.java
+++ Modele_de_jeu/src/ihm/IHMCabane.java
@@ -13,16 +13,12 @@ import devintAPI.Preferences;
 @SuppressWarnings("serial")
 public class IHMCabane extends JButton {
 	IHMCabane() {
-		super("Cabane");
+		super();
 		Preferences pref = Preferences.getData();
 		setLayout(new BorderLayout());
-		setPreferredSize(new Dimension(105, 105));
-		setBackground(pref.getCurrentBackgroundColor());
-		setForeground(pref.getCurrentForegroundColor());
-
-		setFont(new Font("Tahoma", 1, 36));
-		setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-		setOpaque(true);
+		setPreferredSize(new Dimension(110, 110));
+		setOpaque(false);
+		setBorderPainted(false);
+		setContentAreaFilled(false);
 	}
 }
